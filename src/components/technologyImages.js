7e0617a7c152a0def1ece4184bad5375654c3891
Louie Laureles <44080866+ruuuuuuuiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-03-30 19:42:56 +0800

    added new technology for display

--- src/components/technologyImages.js
+++ src/components/technologyImages.js
@@ -2,9 +2,11 @@ import bootstrap from "../images/technology/icons8-bootstrap-100.png";
 import cplus from "../images/technology/icons8-c++-100.png";
 import c from "../images/technology/icons8-c-programming-100.png";
 import css from "../images/technology/icons8-css3-100.png";
+import docker from "../images/technology/icons8-docker-100.png";
 import express from "../images/technology/icons8-express-js-100_1.png";
 import github from "../images/technology/icons8-github-100_1.png";
 import html from "../images/technology/icons8-html-5-100.png";
+import ionic from "../images/technology/icons8-ionic-100.png";
 import java from "../images/technology/icons8-java-100.png";
 import javascript from "../images/technology/icons8-javascript-100.png";
 import jwt from "../images/technology/icons8-json-web-token-100.png";
@@ -16,28 +18,135 @@ import postman from "../images/technology/icons8-postman-is-the-only-complete-ap
 import react from "../images/technology/icons8-react-native-100.png";
 import redux from "../images/technology/icons8-redux-100.png";
 import spring from "../images/technology/icons8-spring-logo-100.png";
+import sqlserver from "../images/technology/icons8-sql-server-100.png";
 import tailwind from "../images/technology/icons8-tailwindcss-100.png";
 import tomcat from "../images/technology/icons8-tomcat-100.png";
+import typescript from "../images/technology/icons8-typescript-100.png";
 
-export default [
-  bootstrap,
-  cplus,
-  c,
-  css,
-  express,
-  github,
-  html,
-  java,
-  javascript,
-  jwt,
-  mui,
-  mongodb,
-  mysql,
-  nodejs,
-  postman,
-  react,
-  redux,
-  spring,
-  tailwind,
-  tomcat,
-];
+const image = [
+  {
+    altName: 'Bootstrap', 
+    imgData: bootstrap,
+  },
+  {
+    altName: 'C++', 
+    imgData: cplus,
+  },
+  {
+    altName: 'C', 
+    imgData: c,
+  },
+  {
+    altName: 'CSS', 
+    imgData: css,
+  },
+  {
+    altName: 'Docker', 
+    imgData: docker,
+  },
+  {
+    altName: 'Express', 
+    imgData: express,
+  },
+  {
+    altName: 'Github', 
+    imgData: github,
+  },
+  {
+    altName: 'HTML', 
+    imgData: html,
+  },
+  {
+    altName: 'Ionic', 
+    imgData: ionic,
+  },
+  {
+    altName: 'Java', 
+    imgData: java,
+  },
+  {
+    altName: 'JavaScript', 
+    imgData: javascript,
+  },
+  {
+    altName: 'JWT', 
+    imgData: jwt,
+  },
+  {
+    altName: 'MUI', 
+    imgData: mui,
+  },
+  {
+    altName: 'MongoDB', 
+    imgData: mongodb,
+  },
+  {
+    altName: 'MySQL', 
+    imgData: mysql,
+  },
+  {
+    altName: 'NodeJS', 
+    imgData: nodejs,
+  },
+  {
+    altName: 'Postman', 
+    imgData: postman,
+  },
+  {
+    altName: 'React', 
+    imgData: react,
+  },
+  {
+    altName: 'Redux', 
+    imgData: redux,
+  },
+  {
+    altName: 'Spring Boot', 
+    imgData: spring,
+  },
+  {
+    altName: 'SQL Server', 
+    imgData: sqlserver,
+  },
+  {
+    altName: 'Tailwind CSS', 
+    imgData: tailwind,
+  },
+  {
+    altName: 'Tomcat', 
+    imgData: tomcat,
+  },
+  {
+    altName: 'TypeScript', 
+    imgData: typescript,
+  },
+]
+
+// export default [
+//   bootstrap,
+//   cplus,
+//   c,
+//   css,
+//   docker,
+//   express,
+//   github,
+//   html,
+//   ionic,
+//   java,
+//   javascript,
+//   jwt,
+//   mui,
+//   mongodb,
+//   mysql,
+//   nodejs,
+//   postman,
+//   react,
+//   redux,
+//   spring,
+//   sqlserver,
+//   tailwind,
+//   tomcat,
+//   typescript,
+// ];
+
+export default image;
