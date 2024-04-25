167eb6522b4a30ff230ff5e349e39678ad8ed93b
Louie Laureles <44080866+ruuuuuuuiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2024-04-25 08:55:26 +0800

    feat: theme - dark & light prep

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,9 @@ module.exports = {
     extend: {
       colors: {
         "deep-blue": "#010026",
-        blue: "#2CBCE9",
-        red: "#DC4492",
-        yellow: "#FDCC49",
+        blue: "rgba(var(--blue))",
+        red: "rgba(var(--red))",
+        yellow: "rgba(var(--yellow))",
         grey: "#ededed",
         "light-violet": "#FC59FF",
         "dark-grey": "#757575",
